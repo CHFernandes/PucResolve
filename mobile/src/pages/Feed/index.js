b35095208444a46adcbc749c9e482cfb462637c7
Carlos Henrique <chfernandes01@hotmail.com>
2020-06-16 20:41:18 -0300

    principios de funcionalidade do feed

--- mobile/src/pages/Feed/index.js
+++ mobile/src/pages/Feed/index.js
@@ -8,47 +8,88 @@ import api from '../../services/api';
 import styles from './styles';
 
 export default function Feed(){
+    const navigation = useNavigation();
     const routes = useRoute();
     const header = routes.params.authorization;
+
+    const [incidents, setIncidents] = useState([]);
+    const [user, setUser] = useState("usuário");
+
+    function upvote(){
+
+    }
+
+    function downvote(){
+
+    }
+
+    function countVotes(){
+
+    }
+
+    function logout(){
+        navigation.navigate('Login');
+    }
+
+    async function findUser(user){
+        if(!header){
+            logout();
+        }else{
+            const url = `/getUser/${user}`;
+            const response = await api.post(`${url}`);
+            return response.data;
+        }
+    }
+
+    async function loadIncidents(){
+        const authenticatedUser = await findUser(header);
+        setUser(authenticatedUser);
+        const response = await api.get('showAll');
+        setIncidents(response.data.docs);
+    }
+
+    useEffect(() => {
+        loadIncidents();
+    }, []);
 
     return(
         <View style={styles.container}>
             <View style={styles.header}>
                 <View style={styles.headerLeft}>
                     <Text style={styles.welcome}>Bem Vindo: </Text>
-                    <Text style={styles.user}>usuário</Text>
+                    <Text style={styles.user}>{user}</Text>
                 </View>
-                <TouchableOpacity style={styles.logoutBtn}>
+                <TouchableOpacity style={styles.logoutBtn} onPress={logout}>
                     <Text style={styles.logoutText}>Sair</Text>
                     <Feather name='log-out' size={25} color='#fb5b5a' />
                 </TouchableOpacity>              
             </View>
             <View style={styles.incidentList}>
                 <FlatList
-                    data={[1,2,3,4,5,6,7,8]}
-                    keyExtractor={incident => String(incident)}
+                    data={incidents}
+                    keyExtractor={incident => String(incident._id) }
                     showsVerticalScrollIndicator={false}
-                    renderItem={() => (
+                    renderItem={({ item: incident}) => (
                         <View style={styles.incident}>
                             <View style={styles.incidentLeft}>
                                 <View style={styles.incidentHeader}>
-                                    <Text style={styles.incidentDesc}>Vaso do banheiro masculino do segundo andar está entupido</Text>
-                                    <Text style={styles.incidentDate}>11/06/2020</Text>
+                                    <Text style={styles.incidentDesc}>{incident.descricao}</Text>
+                                    <Text style={styles.incidentDate}>{incident.data}</Text>
                                 </View>
                                 <View style={styles.incidentFooter}>
-                                    <Text style={styles.incidentLoc}>Bloco 2</Text>
+                                    <Text style={styles.incidentLoc}>{incident.localizacao}</Text>
                                     <View style={styles.incidentRegister}>
                                         <Text style={styles.incidentRegisterTitle}>Registrado por: </Text>
-                                        <Text style={styles.incidentRegisterUser}>Pedro Pedroso</Text>
+                                        <Text style={styles.incidentRegisterUser}>{incident.criador}</Text>
                                     </View>
                                 </View>
                             </View>
                             <View style={styles.incidentRight}>
-                                <TouchableOpacity style={styles.upvote}>
+                                <TouchableOpacity style={styles.upvote} onPress={upvote}>
                                     <Feather name='arrow-up' size={40} color='#fff'/>
                                 </TouchableOpacity>
                                 <Text style={styles.votesCounter}>0</Text>
-                                <TouchableOpacity style={styles.downvote}>
+                                <TouchableOpacity style={styles.downvote} onPress={downvote}>
                                     <Feather name='arrow-down' size={40} color='#fff'/>
                                 </TouchableOpacity>
                             </View>
